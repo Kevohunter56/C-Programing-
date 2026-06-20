00ce0526ed99367cd72c121fc1293d0059b840da
Kevohunter56 <kevinssemakula56@gmail.com>
2026-06-20 22:59:28 +0300

    Updated code

--- main.c
+++ main.c
@@ -1,19 +1,53 @@
+#include <stdio.h>
 #include "glfw3.h"
+
+
+void framebuffer_size_callback(GLFWwindow* window,int width,  int height)
+{
+    glViewport(0, 0, width, height);
+}
+
 
 int main()
 {
     glfwInit();
+    
+    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
 
     GLFWwindow* window =
-        glfwCreateWindow(800, 600, "Hello Great GLFW", NULL, NULL);
+        glfwCreateWindow(mode->width, mode->height, "C PROGRAMMING WITH GLFW OPENGL", primaryMonitor, NULL);
+    
+    int WindowWidth, WindowHeight;
+
 
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
+    // Make the OpenGL context current
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     while (!glfwWindowShouldClose(window))
+
     {
+        // Get the current window size
+        glfwGetFramebufferSize(window, &WindowWidth, &WindowHeight);
+        glViewport(0, 0, WindowWidth, WindowHeight);
+  
+        
+        // Set background color (Red, Green, Blue, Alpha)
+        glClearColor(0.0f, 0.2f, 0.2f, 1.0f);
+
+        // Clear the color buffer
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Display what was drawn
+        glfwSwapBuffers(window);
+
+        // Process events
         glfwPollEvents();
     }
 
